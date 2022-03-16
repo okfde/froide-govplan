475f2fe4c11ad5c756eeca1ddf8e0fcbb34b995d
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-03-16 13:10:49 +0100

    Reword request template

--- froide_govplan/utils.py
+++ froide_govplan/utils.py
@@ -12,7 +12,7 @@ def make_request_url(plan, publicbody):
     subject = "Stand des Regierungsvorhabens „{}“".format(plan.title)
     if len(subject) > 250:
         subject = subject[:250] + "..."
-    body = "Dokumente, die den Stand des Regierungsvorhabens „{}“ (siehe Koalitionsvertrag), dokumentieren.".format(
+    body = "Dokumente, die den Stand des Regierungsvorhabens zum Thema {} (siehe Koalitionsvertrag), dokumentieren.".format(
         plan.title
     )
     query = {
