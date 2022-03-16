e7d5496254935a0b1440afbcb09b4b4015f3d923
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-03-16 13:11:02 +0100

    Add toolbar item on main govplan page

--- froide_govplan/cms_toolbars.py
+++ froide_govplan/cms_toolbars.py
@@ -7,22 +7,30 @@ from cms.toolbar_pool import toolbar_pool
 
 class GovPlanToolbar(CMSToolbar):
     def populate(self):
-        if not hasattr(self.request, "govplan"):
+        if self.request.current_page.application_namespace != "govplan":
             return
+        menu = self.toolbar.get_or_create_menu("govplan-menu", "Koalitionstracker")
 
-        menu = self.toolbar.get_or_create_menu("govplan-menu", _("GovPlan"))
-        govplan = self.request.govplan
         url = reverse(
-            "admin:froide_govplan_governmentplan_change",
-            args=(govplan.pk,),
+            "admin:app_list",
+            kwargs={"app_label": "froide_govplan"},
             current_app="govplanadmin",
         )
-        menu.add_modal_item(_("Edit government plan"), url=url)
-        url = reverse(
-            "admin:froide_govplan_governmentplanupdate_add",
-            current_app="govplanadmin",
-        )
-        menu.add_modal_item(_("Add update"), url=url)
+        menu.add_modal_item(_("Edit plans and updates"), url=url)
+
+        if hasattr(self.request, "govplan"):
+            govplan = self.request.govplan
+            url = reverse(
+                "admin:froide_govplan_governmentplan_change",
+                args=(govplan.pk,),
+                current_app="govplanadmin",
+            )
+            menu.add_modal_item(_("Edit government plan"), url=url)
+            url = reverse(
+                "admin:froide_govplan_governmentplanupdate_add",
+                current_app="govplanadmin",
+            )
+            menu.add_modal_item(_("Add update"), url=url)
 
 
 toolbar_pool.register(GovPlanToolbar)
