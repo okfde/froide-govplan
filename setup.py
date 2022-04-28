da21081be0e76f308597f30bfda97cdd1a6a1b6f
Karl Engelhardt <karl.engelhardt@okfn.de>
2022-04-28 15:20:50 +0200

    🧑‍💻 Add pre-commit

--- setup.py
+++ setup.py
@@ -2,23 +2,22 @@
 
 from __future__ import print_function
 
+import codecs
 import os
 import re
-import codecs
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 
 def read(*parts):
     filename = os.path.join(os.path.dirname(__file__), *parts)
-    with codecs.open(filename, encoding='utf-8') as fp:
+    with codecs.open(filename, encoding="utf-8") as fp:
         return fp.read()
 
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
@@ -27,26 +26,26 @@ def find_version(*file_paths):
 setup(
     name="froide_govplan",
     version=find_version("froide_govplan", "__init__.py"),
-    url='https://github.com/okfde/froide-govplan',
-    license='MIT',
+    url="https://github.com/okfde/froide-govplan",
+    license="MIT",
     description="Froide govplan app",
-    long_description=read('README.md'),
-    author='Stefan Wehrmeyer',
-    author_email='mail@stefanwehrmeyer.com',
+    long_description=read("README.md"),
+    author="Stefan Wehrmeyer",
+    author_email="mail@stefanwehrmeyer.com",
     packages=find_packages(),
     install_requires=[
-        'froide',
+        "froide",
     ],
     include_package_data=True,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
     ],
     zip_safe=False,
 )
